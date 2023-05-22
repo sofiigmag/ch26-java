1cc277dc0a37bd04f9e5f5992668eae433c84aae
sofiigmag <sofia.gmagk@gmail.com>
2023-05-22 17:32:41 -0600

    2da parte ejemplo cajeros

---
+++ java09-OOP/src/com/santander/Bank.java
@@ -0,0 +1,35 @@
+package com.santander;
+
+import java.util.ArrayList;
+
+public class Bank {
+	String address;
+	int sucursalNumber;
+	ArrayList<ATM> ATMs; // = new ArrayList<>();
+	{
+		ATMs = new ArrayList<>();
+	}
+	
+	static int counterBank = 1;
+	
+	Bank(){
+		sucursalNumber = counterBank++;	
+	}
+	
+	Bank(String address){
+		this(); // invoca al constructor por default
+		this.address = address;
+	}
+	
+	String getATMs() {
+		String text = "";
+		for (ATM Atm : ATMs ) {
+			text += Atm.serialNumber + "  $" + Atm.balance + "\n";
+		}
+		return text;
+		
+	}
+
+	
+
+}
